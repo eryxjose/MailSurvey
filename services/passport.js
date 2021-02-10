dd11e3a8ac05f3a9e3c369beb0fedb8feb601ebf
eryxjose <eryxjose@hotmail.com>
2021-02-09 23:13:17 -0300

    Implementados Cookie e lógica de autenticação.

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,18 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => { 
+        done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy(
     {
@@ -11,7 +22,15 @@ passport.use(new GoogleStrategy(
         callbackURL: keys.callbackURL
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        new User({ googleId: profile.id }).save();
+        User.findOne({ googleId: profile.id }).then( existingUser => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        });
+        
     }
 ));
