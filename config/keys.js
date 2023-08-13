b88c2147e574f7686f14b416c5858a062576cb7f
Eryx Jose Cunha Guimaraes Neto <eryx.neto@gsoftware.com.br>
2023-08-13 15:20:54 -0300

    Corrige keys

--- config/keys.js
+++ config/keys.js
@@ -2,20 +2,9 @@
 
 if (process.env.NODE_ENV === 'production') {
     // return production keys
-    //module.exports = require('./prodkeys');
-    module.exports = {
-        googleClientID: process.env.GOOGLE_CLIENT_ID,
-        googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        mongoURI: process.env.MONGO_URI,
-        cookieKey: process.env.COOKIE_KEY
-    };
+    module.exports = require('./prodkeys');
 } else {
     // return dev keys
-    module.exports = {
-        googleClientID: '175922520607-ngrpmiv4gfb3n0q6oe06b93g89tbdjls.apps.googleusercontent.com',
-        googleClientSecret: 'IaMi5gWasief11IdmwGOaCZv',
-        mongoURI: 'mongodb+srv://mailsurvey:OrPNZD07OYl0oW0H@cluster0.owrsn.mongodb.net/?retryWrites=true&w=majority',
-        cookieKey: 'slkajflsdkjlksdjflksjdfwerwer'
-    };
+    module.exports = require('./devkeys');
 }
 
